1a3e7c391f54f3c8993a1e4d812e0a2f6ea944ed
Yang-z <Mr.Y.Zheng@gmail.com>
2021-07-30 00:28:09 +0800

    Update data structure

--- data_structure.h
+++ data_structure.h
@@ -1,70 +1,96 @@
 
-struct dotBox2D
+struct dotBox2d
 {
-    char box2d_version;
+    const char fileFormat[6]{'.', 'b', 'o', 'x', '2', 'd'};
+    short dotBox2dVersion[3];
+    short box2dVersion[3];
 
-    dotB2Wrold *world;
+    // int countB2World{1};
+    int countB2Body;
+    int countB2Fixture;
+    int countB2Vec2;
+
+    int locB2World;
+    int locB2Body;
+    int locB2Fixture;
+    int locB2Vec2;
 };
 
 struct dotB2Wrold
 {
-    float gravity_x{0.0f};
-    float gravity_y{0.0f};
+    dotB2Vec2 gravity{0.0f, 0.0f};
 
-    int count_body;
-    dotB2Body *bodys;
 
-    int count_joint;
-    dotB2Joint *joints;
 };
 
 struct dotB2Body
 {
-    int type{0};
-    float position_x{0.0f};
-    float position_y{0.0f};
-    float angle{0.0f};
-    float linearVelocity_x{0.0f};
-    float linearVelocity_y{0.0f};
-    float angularVelocity{0.0f};
-    float linearDamping{0.0f};
-    float angularDamping{0.0f};
-    bool allowSleep{true};
-    bool awake{true};
-    bool fixedRotation{false};
-    bool bullet{false};
-    bool enabled{true};
-    float gravityScale{1.0f};
+    struct
+    {
+        int type{0};
+        dotB2Vec2 position{0.0f, 0.0f};
+        float angle{0.0f};
+        dotB2Vec2 linearVelocity{0.0f, 0.0f};
+        float angularVelocity{0.0f};
+        float linearDamping{0.0f};
+        float angularDamping{0.0f};
+        bool allowSleep{true};
+        bool awake{true};
+        bool fixedRotation{false};
+        bool bullet{false};
+        bool enabled{true};
+        float gravityScale{1.0f};
 
-    int count;
-    dotB2Fixture *fixtures;
+        struct
+        {
+            int start;
+            int end;
+        } fixtures;
+
+    } raw;
 };
 
 struct dotB2Fixture
 {
+    struct
+    {
+        float friction{0.2f};
+        float restitution{0.0f};
+        float restitutionThreshold{1.0f};
+        float density{0.0f};
+        bool isSensor{false};
 
-    float friction{0.2f};
-    float restitution{0.0f};
-    float restitutionThreshold{1.0f};
-    float density{0.0f};
-    bool isSensor{false};
-    short filter_categoryBits{0x0001};
-    short filter_maskBits{0xFFFF};
-    short filter_groupIndex{0};
+        void *userData{0};
 
-    void *userData{0};
+        struct
+        {
+            short categoryBits{0x0001};
+            short maskBits{0xFFFF};
+            short groupIndex{0};
+        } filter;
 
-    dotB2Shape *shape;
+        struct
+        {
+            int type;
+            int radius;
+
+            struct
+            {
+                int start;
+                int end;
+            } vec2s;
+
+            // bool oneSided;
+
+        } shape;
+
+    } raw;
 };
 
-struct dotB2Shape
+struct dotB2Vec2
 {
-    int type;
-    int radius;
-    bool oneSided;
-
-    int count;
-    float *points;
+    float x;
+    float y;
 };
 
 struct dotB2Joint
